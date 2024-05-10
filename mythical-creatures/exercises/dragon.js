1d9a0dc8b8d580a546d521c565d059fa4e4983ed
seth-way <seth.r.way@gmail.com>
2024-05-10 12:41:47 -0400

    complete dragon functions

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,37 @@
+function createDragon(name, rider, temperment) {
+  return {
+    name,
+    rider,
+    temperment,
+    timesEaten: 0,
+    hungry: true,
+  };
+}
 
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if (dragon.timesEaten > 2) dragon.hungry = false;
+
+  return dragon;
+}
+
+function findFireBreathers(dragons) {
+  var fireBreathers = []
+
+  for (let i = 0; i < dragons.length; i += 1) {
+    if (dragons[i].temperment === 'aggressive') fireBreathers.push(dragons[i])
+  }
+
+  return fireBreathers;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
-}
+  createDragon,
+  greetRider,
+  eat,
+  findFireBreathers,
+};
--- mythical-creatures/test/dragon-test.js
+++ mythical-creatures/test/dragon-test.js
@@ -1,35 +1,39 @@
 var assert = require('chai').assert;
-var { createDragon, greetRider, eat, findFireBreathers} = require('../exercises/dragon');
+var {
+  createDragon,
+  greetRider,
+  eat,
+  findFireBreathers,
+} = require('../exercises/dragon');
 
-describe('Dragon', function() {
-
-  it.skip('should be able to create a dragon with a name', function() {
+describe('Dragon', function () {
+  it('should be able to create a dragon with a name', function () {
     var dragon = createDragon('Jeff');
 
     assert.equal(dragon.name, 'Jeff');
   });
 
-  it.skip('should be able to have a different name', function() {
+  it('should be able to have a different name', function () {
     var dragon = createDragon('Louisa');
 
     assert.equal(dragon.name, 'Louisa');
   });
 
-  it.skip('should have a rider', function() {
+  it('should have a rider', function () {
     var dragon = createDragon('Saphira', 'Eragon');
 
     assert.equal(dragon.name, 'Saphira');
     assert.equal(dragon.rider, 'Eragon');
   });
 
-  it.skip('should be able to have a different rider', function() {
+  it('should be able to have a different rider', function () {
     var dragon = createDragon('Elliot', 'Pete');
 
     assert.equal(dragon.name, 'Elliot');
     assert.equal(dragon.rider, 'Pete');
   });
 
-  it.skip('should have a temperment', function() {
+  it('should have a temperment', function () {
     var dragon = createDragon('Saphira', 'Eragon', 'gentle');
 
     assert.equal(dragon.name, 'Saphira');
@@ -37,10 +41,9 @@ describe('Dragon', function() {
     assert.equal(dragon.temperment, 'gentle');
   });
 
-  it.skip('should be able to have different temperments', function() {
+  it('should be able to have different temperments', function () {
     var dragon1 = createDragon('Gray', 'Marley', 'aggressive');
     var dragon2 = createDragon('Sky', 'Susie', 'gentle');
-
 
     assert.equal(dragon1.name, 'Gray');
     assert.equal(dragon1.rider, 'Marley');
@@ -51,7 +54,7 @@ describe('Dragon', function() {
     assert.equal(dragon2.temperment, 'gentle');
   });
 
-  it.skip('should greet their rider', function() {
+  it('should greet their rider', function () {
     var dragon1 = createDragon('Gray', 'Marley', 'aggressive');
     var dragon2 = createDragon('Sky', 'Susie', 'gentle');
 
@@ -62,19 +65,19 @@ describe('Dragon', function() {
     assert.equal(greeting2, 'Hi, Susie!');
   });
 
-  it.skip('should start off having eaten 0 times', function() {
+  it('should start off having eaten 0 times', function () {
     var dragon = createDragon('Mushu', 'Mulan', 'aggressive');
 
     assert.equal(dragon.timesEaten, 0);
   });
 
-  it.skip('should start off being hungry', function() {
+  it('should start off being hungry', function () {
     var dragon = createDragon('Mushu', 'Mulan', 'aggressive');
 
     assert.equal(dragon.hungry, true);
   });
 
-  it.skip('should be full after eating 3 times', function() {
+  it('should be full after eating 3 times', function () {
     var dragon = createDragon('Lady Vox', 'Emily', 'gentle');
 
     var fedDragon = eat(dragon);
@@ -94,16 +97,16 @@ describe('Dragon', function() {
   });
 
   //Spicy:
-  it.skip('should be a fireBreather if aggressive in temperment', function() {
+  it('should be a fireBreather if aggressive in temperment', function () {
     var dragon1 = createDragon('Gray', 'Marley', 'aggressive');
     var dragon2 = createDragon('Sky', 'Susie', 'gentle');
     var dragon3 = createDragon('Mushu', 'Mulan', 'aggressive');
     var dragon4 = createDragon('Lady Vox', 'Emily', 'gentle');
 
     var allDragons = [dragon1, dragon2, dragon3, dragon4];
-    
+
     var fireBreathers = findFireBreathers(allDragons);
 
-    assert.deepEqual(fireBreathers, [dragon1, dragon3])
+    assert.deepEqual(fireBreathers, [dragon1, dragon3]);
   });
 });
