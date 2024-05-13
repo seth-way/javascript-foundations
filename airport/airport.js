0f3ff98cec92fd8e79961ecb75f30a0399bac664
seth-way <seth.r.way@gmail.com>
2024-05-13 11:26:32 -0400

    complete airport functions

--- airport/airport.js
+++ airport/airport.js
@@ -1,14 +1,35 @@
-function createAirport() {}
+function createAirport(name, airlines, availableGates) {
+  return { name, airlines, availableGates };
+}
 
-function welcomeGuests() {}
+function welcomeGuests(airport) {
+  return `Welcome to ${airport.name}!`;
+}
 
-function landPlanes() {}
+function landPlanes(airport, numberOfPlanes) {
+  airport.availableGates -= numberOfPlanes;
+  if (airport.availableGates < 0) {
+    airport.message = `Oh no! Not enough gates available. Current overflow is ${-airport.availableGates}.`;
+    airport.availableGates = 0;
+  } else {
+    airport.message = `Success! Current availability is ${airport.availableGates}.`;
+  }
+  return airport;
+}
 
-function checkAirlineLocations() {}
+function checkAirlineLocations(airports, carrier) {
+  const locations = [];
+
+  for (const airport of airports) {
+    if (airport.airlines.includes(carrier)) locations.push(airport.name);
+  }
+
+  return locations;
+}
 
 module.exports = {
   createAirport,
   welcomeGuests,
   landPlanes,
-  checkAirlineLocations
+  checkAirlineLocations,
 };
