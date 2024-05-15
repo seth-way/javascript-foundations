643a9ef404b89dd4cb5ab83a26873d7632968d94
seth-way <seth.r.way@gmail.com>
2024-05-15 17:32:16 -0400

    update readme progress

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,17 @@
+function createElevator(building, floors, currentFloor, passengers) {
+  return { building, floors, currentFloor, passengers };
+}
 
+function changeFloors(elevator, floor) {
+  if (elevator.currentFloor === floor) {
+    return `You're already on floor ${floor}!`
+  }
 
-module.exports = {  };
+  if (floor > elevator.floors || floor < 1) {
+    return `Floor ${floor} does not exist!`
+  }
+}
+
+function dropOffPassenger() {}
+
+module.exports = { createElevator, changeFloors, dropOffPassenger };
