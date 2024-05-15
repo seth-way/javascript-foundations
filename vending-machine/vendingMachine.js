94e514700e9dce8c2d6a87f07c7a1deeab28a16e
seth-way <seth.r.way@gmail.com>
2024-05-15 17:07:47 -0400

    complete vending machine functions

--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,33 @@
+function createItemStock(name = 'unknown', quantity = 0, price = 1.0) {
+  return { name, quantity, price };
+}
 
-module.exports = {}
+// function collectChange(coins) {
+//   var total = 0;
+
+//   for (let i = 0; i < coins.length; i += 1) {
+//     total += coins[i];
+//   }
+
+//   return total;
+// }
+
+function collectChange(coins) {
+  return coins.reduce(function (acc, val) {
+    return acc + val;
+  }, 0);
+}
+
+function makePurchase(selectedItem, moneyForPurchase) {
+  if (selectedItem.price > moneyForPurchase) {
+    return `Sorry, you need at least $${~~selectedItem.price} to make that purchase`;
+  }
+
+  if (!selectedItem.quantity) {
+    return 'Sorry, there are none left';
+  }
+
+  return `Here are your ${selectedItem.name}`;
+}
+
+module.exports = { createItemStock, collectChange, makePurchase };
