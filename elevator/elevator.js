a1a4678cd1798c31341e9a8f567ffa160af40a89
seth-way <seth.r.way@gmail.com>
2024-05-15 17:38:18 -0400

    complete elevator functions

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -4,14 +4,23 @@ function createElevator(building, floors, currentFloor, passengers) {
 
 function changeFloors(elevator, floor) {
   if (elevator.currentFloor === floor) {
-    return `You're already on floor ${floor}!`
+    return `You're already on floor ${floor}!`;
   }
 
   if (floor > elevator.floors || floor < 1) {
-    return `Floor ${floor} does not exist!`
+    return `Floor ${floor} does not exist!`;
   }
+
+  return `Taking you to floor ${floor}!`;
 }
 
-function dropOffPassenger() {}
+function dropOffPassenger(elevator, passenger) {
+  var passengers = elevator.passengers;
+  var passengerIndex = passengers.indexOf(passenger);
+
+  if (passengerIndex >= 0) passengers.splice(passengerIndex, 1);
+  
+  return passengers;
+}
 
 module.exports = { createElevator, changeFloors, dropOffPassenger };
