302ecda5108f394f83b5fdbbd8ee9eb0d193d984
seth-way <seth.r.way@gmail.com>
2024-05-15 16:27:56 -0400

    complete video game functions

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,21 @@
+function createPlayer(name, age, moveset) {
+  return { name, age, moveset };
+}
 
+function createLevel(name, players) {
+  return { name, players, coins: 0, lives: 3 };
+}
 
+function findCoins(level, coinCount) {
+  level.coins += coinCount;
+  if (level.coins >= 100) level.lives += 1;
+  return level;
+}
 
-module.exports = { };
+function defeatPlayer(level) {
+  level.lives -= 1;
+  if (level.lives) return level;
+  return 'GAME OVER';
+}
+
+module.exports = { createPlayer, createLevel, findCoins, defeatPlayer };
