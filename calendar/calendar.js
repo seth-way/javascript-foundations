eafb2a8e53af583d1ad5cb9001dc3352971319fa
seth-way <seth.r.way@gmail.com>
2024-05-15 16:48:14 -0400

    complete calendar functions

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,20 @@
+function createEvent(title, month, day) {
+  if (day < 1 || day > 31) return `Error: ${day} is not a valid day`;
+  return { title, month, day };
+}
 
+function createCalendar(owner, events) {
+  return { owner, events };
+}
 
-module.exports = {  };
+function reportMonthlyEvents(calendar, month) {
+  const events = [];
+
+  for (let i = 0; i < calendar.events.length; i += 1) {
+    if (calendar.events[i].month === month) events.push(calendar.events[i]);
+  }
+
+  return events;
+}
+
+module.exports = { createEvent, createCalendar, reportMonthlyEvents };
