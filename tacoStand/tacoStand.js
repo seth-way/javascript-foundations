f39894a1ebdda08526d6e721d0f1ab9bd60f882f
seth-way <seth.r.way@gmail.com>
2024-05-17 11:49:27 -0400

    complete taco functions

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,32 @@
+function createIngredient(name = 'unknown', price = 0.00) {
+  return { name, price };
+}
 
-module.exports = {}
+function createTaco(name = 'custom', ingredients = []) {
+  return {
+    name,
+    ingredients,
+  };
+}
+
+function addIngredientToTaco(taco, ingredient) {
+  if (ingredient) taco.ingredients.push(ingredient);
+  return taco;
+}
+
+function calculatePrice(taco) {
+  var price = 0;
+
+  for (let i = 0; i < taco.ingredients.length; i += 1) {
+    price += taco.ingredients[i].price;
+  }
+
+  return price;
+}
+
+module.exports = {
+  createIngredient,
+  createTaco,
+  addIngredientToTaco,
+  calculatePrice,
+};
