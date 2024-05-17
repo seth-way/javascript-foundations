32ae6c29426541d61ae555949b54850800305f7b
seth-way <seth.r.way@gmail.com>
2024-05-17 12:36:53 -0400

    complete barber functions

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,27 @@
+function createBarber(name, earnings = 0, haircuts = []) {
+  return { name, earnings, haircuts };
+}
 
+function giveCompliment(haircut) {
+  return `This ${haircut.style} looks great!`;
+}
 
-module.exports = {  };
+function cutHair(barber, haircut) {
+  barber.haircuts.push(haircut);
+  barber.earnings += haircut.price;
+  return barber;
+}
+
+function listStyles(barber, cutLength) {
+  var styleOptions = [];
+
+  for (let i = 0; i < barber.haircuts.length; i += 1) {
+    if (barber.haircuts[i].hairLength === cutLength) {
+      styleOptions.push(barber.haircuts[i].style);
+    }
+  }
+  
+  return styleOptions;
+}
+
+module.exports = { createBarber, giveCompliment, cutHair, listStyles };
