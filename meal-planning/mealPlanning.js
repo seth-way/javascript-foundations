c1bc3ab669725c7d620cea3e056be0a754a335f8
seth-way <seth.r.way@gmail.com>
2024-05-17 12:46:31 -0400

    complete meal-planning functions

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,21 @@
+function createMeal(type, calorieGoal, dishes = []) {
+  return { type, calorieGoal, dishes };
+}
 
-module.exports = { }
+function addDish(meal, dish) {
+  if (dish.calories < meal.calorieGoal) {
+    meal.dishes.push(dish);
+    meal.calorieGoal -= dish.calories;
+  }
+  return meal;
+}
+
+function calculateCalories(meal) {
+  var calories = 0;
+  for (let i = 0; i < meal.dishes.length; i += 1 ) {
+    calories += meal.dishes[i].calories
+  }
+  return `${meal.type} has a total of ${calories} calories.`
+}
+
+module.exports = { createMeal, addDish, calculateCalories };
